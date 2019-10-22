32a8c60f46c4b0edb405316771fc59c6f702098e
Niks <nikhilesh.patve@gmail.com>
2019-10-22 09:50:18 +0530

    try

--- app/src/main/java/com/niks/animationdemo/MainActivity.kt
+++ app/src/main/java/com/niks/animationdemo/MainActivity.kt
@@ -2,6 +2,8 @@ package com.niks.animationdemo
 
 import android.animation.ObjectAnimator
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.util.DisplayMetrics
 import android.util.Log
 import android.view.View
@@ -11,57 +13,68 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    var clickedCount = 1
+    var clickedCount = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        printViews()
 
 
         startAnimation.setOnClickListener {
-            //printCoordinates(fir)
-            // when (clickedCount) {
-            //     0 -> {
-            //
-            //     }
-            //
-            //     1 -> {
-            //
-            //     }
-            //     2 -> {
-            //
-            //     }
-            //
-            //     3 -> {
-            //
-            //     }
-            // }
-
             startAnimation(firstView, getYDelta(clickedCount, 0, firstView))
             startAnimation(secondView, (getYDelta(clickedCount + 1, 1, secondView)))
-            printCoordinates(secondView)
             startAnimation(thirdView, (getYDelta(clickedCount + 2, 2, thirdView)))
             startAnimation(fourthView, (getYDelta(clickedCount + 3, 3, fourthView)))
-
+            printViews()
             clickedCount++
-
         }
+    }
+
+    private fun printViews() {
+        Handler(Looper.getMainLooper()).postDelayed({
+            printCoordinates(firstView)
+            printCoordinates(secondView)
+            printCoordinates(thirdView)
+            printCoordinates(fourthView)
+            Log.d("yep", "=============")
+        }, 2000)
     }
 
     private fun getYDelta(clickedCount: Int, position: Int, view: View): Float {
-        return if (clickedCount % 4 == position) {
-            0F
+        if (clickedCount % 4 == 0) {
+            return when (clickedCount % 4) {
+                0 -> {
+                    100F
+                }
+                1 -> {
+                    200F
+                }
+                2 -> {
+                    300F
+                }
+                3 -> {
+                    0F
+                }
+                else -> {
+                    0F
+                }
+            }
         } else {
-            view.y + dpToPx(60).toFloat()
+            return if (view.y == 300F) {
+                return -300F
+            } else {
+                100F
+            }
         }
     }
 
-    fun dpToPx(dp: Int): Int {
+    private fun dpToPx(dp: Int): Int {
         val displayMetrics = resources.displayMetrics
         return Math.round(dp * (displayMetrics.xdpi / DisplayMetrics.DENSITY_DEFAULT))
     }
 
-    fun startAnimation(view: View, yTranslation: Float) {
+    private fun startAnimation(view: View, yTranslation: Float) {
         val objectAnimator = ObjectAnimator.ofFloat(view, "translationY", yTranslation)
         objectAnimator.interpolator = AccelerateDecelerateInterpolator()
         objectAnimator.duration = 1000
@@ -69,6 +82,6 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun printCoordinates(view: View) {
-        Log.d("yep", "X = ${view.x} Y = ${view.y}")
+        Log.d("yep", "X = ${view.x} Y = ${view.y} $view")
     }
 }
